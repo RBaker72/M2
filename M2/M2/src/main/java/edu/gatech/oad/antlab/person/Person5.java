285d46bfbede173a35713a58b856727998bcaa73
Unknown <ebrook3@gatech.edu>
2017-09-04 15:49:09 -0400

    Person5Changes
    I wrote the code, and now it'll take a String and shift it over by two spaces to the left

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Evan Brook
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  int length = input.length();
+	  String subA = input.substring(2, length);
+	  String subB = input.substring(0, 2);
+	  return (subA + subB);
 	}
 	
 	/**
