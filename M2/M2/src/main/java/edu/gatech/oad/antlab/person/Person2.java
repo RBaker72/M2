f8edd2286ed74affe3290c1ad89716ced4169c0c
Unknown <rbaker72@gatech.edu>
2017-09-05 16:53:01 -0400

    Added P2 code
    added code for Person 2

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,13 @@
+
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Regina Baker
  * @version 1.1
  */
 public class Person2 {
@@ -30,8 +32,20 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+	  char a[] = input.toCharArray();
+	  Random random = new Random();
+      
+    
+      for( int i = 0 ; i < a.length ; i++ ){
+        int j = random.nextInt(a.length);
+        // Swap letters
+        char temp = a[i]; 
+        a[i] = a[j];
+        a[j] = temp;
+        }       
+
+      return new String( a );
+	  //return null;
 	}
 	/**
 	 * Return a string rep of this object
